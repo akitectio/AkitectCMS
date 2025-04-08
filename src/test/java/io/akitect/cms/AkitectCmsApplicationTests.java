9391945c79d661dde496c5ace46fd0349110af44
DUY TRAN <tdduy.dev@gmail.com>
2025-04-08 14:33:04 +0700

    first commit

---
+++ src/test/java/io/akitect/cms/AkitectCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.akitect.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkitectCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
